fb83d5f46d26dba80c833b58ccf658bb8efcaf3f
wheres222 <ledger781@proton.me>
2026-03-22 01:55:49 +0100

    Update Discord invite link

--- src/lib/links.ts
+++ src/lib/links.ts
@@ -1,4 +1,4 @@
-export const DISCORD_INVITE_URL = "https://discord.gg/cheatparadise";
+export const DISCORD_INVITE_URL = "https://discord.gg/uGAVn2akEk";
 
 export function getDiscordUrl(): string {
   return DISCORD_INVITE_URL;
